b3aa84b12102e08fa1ec8d4aeebc159cdea02464
Vladyslav Bynytskyy <bynytskyy@semalt.com>
2020-10-27 17:50:27 +0200

    Updates

---
+++ precache-manifest.238ec2587a1c66132cd48d578ec0bb83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1168697657170bcfa84470ad6694cd31",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "850a8132f66de79c6c12",
+    "url": "/millionaire/static/css/main.be322e3e.chunk.css"
+  },
+  {
+    "revision": "a36a2d66c3ab7963aedc",
+    "url": "/millionaire/static/js/2.a5be9121.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/millionaire/static/js/2.a5be9121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850a8132f66de79c6c12",
+    "url": "/millionaire/static/js/main.264bb3b6.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  }
+]);
